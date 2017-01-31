2c764ef95bffaaac124e65976426367aceb475b4
JongWan Hong <serverwizard@naver.com>
2017-01-31 13:34:42 +0900

    홍종완

--- src/controls/MemberAddController.java
+++ src/controls/MemberAddController.java
@@ -24,9 +24,9 @@ public class MemberAddController implements Controller {
 
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		if(model.get("member") == null){ // 입력폼을 요청할 때
+		if(model.get("member") == null){ // 입력폼을 요청할 때: GET 요청으로 간주
 			return "/member/MemberForm.jsp";
-		} else { // 회원 등록을 요청할 때
+		} else { // 회원 등록을 요청할 때: POST 요청으로 간주
 			MemberDao memberDao = (MemberDao) model.get("memberDao");
 			Member member = (Member) model.get("member");
 			memberDao.insert(member);
